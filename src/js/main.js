b8bfba2b0296437f4c6afdc242285121897b5086
motoki-shun <motoki-shun@kayac.com>
2015-11-18 23:48:12 +0900

    es6サンプルコード追加

--- src/js/main.js
+++ src/js/main.js
@@ -23,3 +23,24 @@
   };
   
 })(this, document);
+
+
+
+class Hoge {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+  exec() {
+    console.log(this.a, this.b);
+  }
+}
+
+var hoge = new Hoge(1, 2);
+hoge.exec();
+
+var fuga = () => {
+  console.log("this is submodule kasnuu");
+};
+
+module.exports = fuga;
