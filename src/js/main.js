0b6d183f08e6d16eae3d6476f3fe05bf5f23c40b
motoki-shun <motoki-shun@kayac.com>
2015-12-02 09:44:58 +0900

    concatバージョンにする

--- src/js/main.js
+++ src/js/main.js
@@ -23,24 +23,3 @@
   };
   
 })(this, document);
-
-
-
-class Hoge {
-  constructor(a, b) {
-    this.a = a;
-    this.b = b;
-  }
-  exec() {
-    console.log(this.a, this.b);
-  }
-}
-
-var hoge = new Hoge(1, 2);
-hoge.exec();
-
-var fuga = () => {
-  console.log("this is submodule kasnuu");
-};
-
-module.exports = fuga;
