64e54d1795e0bbfedac0f4e3f4bfcd39f7d64391
motoki-shun <motoki-shun@kayac.com>
2016-03-10 10:53:57 +0900

    main.jsシンプルにしとく

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,2 @@
-import $ from 'jquery';
-// var Util = require('./Util');
-import Util from './Util';
-
-var util = new Util();
-
-class Main {
-  constructor(a, b) {
-    this.a = a;
-    this.b = b;
-  }
-  exec() {
-    console.log(this.a, this.b);
-  }
-}
-
-var main = new Main(1, 2);
-main.exec();
+// import $ from 'jquery';
+// import Util from './Util';
