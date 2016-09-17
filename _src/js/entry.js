f8a3c6002693e755b173a83ef04f79f4fa95ab4c
motoki-shun <motoki-shun@kayac.com>
2016-09-17 20:18:08 +0900

    js構成変更

--- _src/js/entry.js
+++ _src/js/entry.js
@@ -1,6 +1,6 @@
 // import $ from 'jquery';
 // import { EventEmitter2 } from 'eventemitter2';
-// import Util from './Util';
+// import Util from './modules/Util';
 
 // export default class Main extends EventEmitter2 {
 //   constructor(opts = {}) {
